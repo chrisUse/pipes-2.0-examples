2964c37c71e9c6ffb4fdfb83608e337c67c4536d
Linde Christian <linde.chris@googlemail.com>
2012-07-01 18:02:15 +0200

    add example two but dont test it

---
+++ example2.hs
@@ -0,0 +1,26 @@
+{-#LANGUAGE ScopedTypeVariables#-}
+
+{-
+  First example: inspirational by https://github.com/Gabriel439/Haskell-Pipes-Library/issues/6 
+  and http://hackage.haskell.org/packages/archive/pipes/2.0.0/doc/html/Control-Pipe-Common.html
+-}
+
+import Control.Pipe
+import Control.Monad
+-- import Control.Monad.IO.Class
+import Network.CGI
+import Control.Monad.Trans
+import Control.Applicative
+
+check :: (Show a) => Pipe a a IO r
+check = forever $ do
+     x <- await
+     lift $ putStrLn $ "Can '" ++ (show x) ++ "' pass?"
+     ok <- read <$> lift getLine
+     when ok (yield x)
+
+--
+testRunPipe :: IO ()
+testRunPipe = runPipe $ forever await <+< check <+< mapM_ yield [1..]
+--
+--
